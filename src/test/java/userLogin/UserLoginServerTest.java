bc3e21841da1c1bc47417f43194ccf134c8844cd
dalt <dbarrychef@gmail.com>
2021-04-22 18:51:57 +0000

    first commit

---
+++ src/test/java/userLogin/UserLoginServerTest.java
@@ -0,0 +1,14 @@
+package userLogin;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserLoginServerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
